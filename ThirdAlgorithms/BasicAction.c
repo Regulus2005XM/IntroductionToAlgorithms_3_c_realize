b2bcf80313d8e84ff9aad4da693990fba06c8df4
Regulus2005XM <152483851+Regulus2005XM@users.noreply.github.com>
2024-09-23 10:09:29 +0800

    Create BasicAction.c

---
+++ ThirdAlgorithms/BasicAction.c
@@ -0,0 +1,8 @@
+//动态集合操作
+int* Search(int S[], int k);
+void Insert(int S[], int* x);
+void Delete(int S[], int* x);
+int* Minimum(int S[]);
+int* Maximum(int S[]);
+int* SucCessor(int S[], int* x);//寻找大一的元素
+int* PredeCessor(int S[], int* x);//寻找小一的元素
